385106fb2eb707ce20da945e684ba8ddfa8063c3
aivanns <a1vazovski@yandex.ru>
2024-10-22 12:10:26 +0300

    added addUser feature

--- src/components/SettingsTab/SettingsTab.jsx
+++ src/components/SettingsTab/SettingsTab.jsx
@@ -2,11 +2,34 @@ import Cookies from 'universal-cookie';
 import classes from './SettingsTab.module.css';
 import { useEffect, useState } from 'react';
 import { jwtDecode } from 'jwt-decode';
-import { createPortal } from 'react-dom'; // Импортируем Portal
+import { createPortal } from 'react-dom';
+import  axios  from 'axios';
+import { host } from '../../globals'
+
+async function addUser() {
+    const username = document.getElementById('add_username').value;
+    const password = document.getElementById('add_password').value;
+    const group = document.getElementById('add_group').value;
+
+    await axios
+        .post(`http://${host}/user`, {
+            username: username,
+            password: password,
+            group: group
+        },
+        {
+            headers: {
+                "Authorization" : `Bearer ${localStorage.getItem('auth')}`
+            }
+        })
+        .then((response) => {
+            console.log(response)
+        })
+}
 
 export default function SettingsTab() {
     const cookies = new Cookies()
-    const [isModalOpen, setModalOpen] = useState(false); // Состояние для модального окна
+    const [isModalOpen, setModalOpen] = useState(false);
 
     function handleSubmit() {
         const ip = document.getElementById('ipvalue').value
@@ -24,12 +47,10 @@ export default function SettingsTab() {
         localStorage.setItem('port', '443')
     }
 
-    // Функция для открытия модального окна
     function openModal() {
         setModalOpen(true);
     }
 
-    // Функция для закрытия модального окна
     function closeModal() {
         setModalOpen(false);
     }
@@ -62,10 +83,10 @@ export default function SettingsTab() {
                     <div className={classes.modal_header}>
                     <div className={classes.close} onClick={closeModal}>&times;</div>
                     </div>
-                    <form className={classes.user_form}>
-                        <input type="text" placeholder="Имя" className={classes.user_input} />
-                        <input type="password" placeholder="Пароль" className={classes.user_input} />
-                        <input type="text" placeholder="Группа" className={classes.user_input} />
+                    <form className={classes.user_form} onSubmit={(e) => { e.preventDefault(); addUser(); }}>
+                        <input type="text" placeholder="Имя" id='add_username' className={classes.user_input} />
+                        <input type="password" placeholder="Пароль" id='add_password' className={classes.user_input} />
+                        <input type="text" placeholder="Группа" id='add_group' className={classes.user_input} />
                         <button type="submit" className={classes.btn}>Создать</button>
                     </form>
                 </div>
